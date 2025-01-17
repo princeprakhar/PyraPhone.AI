cfbb81ec284e6f6e710229e5cff79703981f8bf7
princeprakhar <prakhardeep173@gmail.com>
2025-01-17 20:57:35 +0530

    updateing in calldashboard

--- frontend/src/components/CallStatusDashboard.jsx
+++ frontend/src/components/CallStatusDashboard.jsx
@@ -247,7 +247,7 @@ function CallStatusDashboard() {
                 <strong>Phone Number:</strong> {callStatus.to_number || "N/A"}
               </p>
               <p className="mt-2 text-gray-400">
-                <strong>Name or Organization name:</strong>{" "}
+                <strong>Name or Organization name:</strong>
                 {callStatus.nameOfOrganization || "N/A"}
               </p>
             </div>
